e2e46689df5fc46232b96121f915d973a272dc7d
Björn Pfoster <bjoern.pfoster@gmail.com>
2017-10-26 16:13:53 +0200

    Added shoppingcart

--- src/View/ShoppingCart/shopping-cart.html.php
+++ src/View/ShoppingCart/shopping-cart.html.php
@@ -1,6 +1,8 @@
 <?php $this->layout('view::Layout/layout.html.php'); ?>
 <?php foreach ($this->v('data') as $name =>$count):?>
     <div class="cart-item">
-        <h3><?= $this->e($name)?> (<?= $this->e($count)?>)</h3>
+        <button class="button inline">Bearbeiten</button>
+        <button class="button inline" style="background-color: red; border: 1px solid darkred">Löschen</button>
+        <h3 class="inline title"><?= $this->e($name)?> (<?= $this->e($count)?>)</h3>
     </div>
 <?php endforeach;?>
