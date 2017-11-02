f58063bf2bba7798a15780729acc9c686712afab
Björn Pfoster <bjoern.pfoster@gmail.com>
2017-11-02 15:37:45 +0100

    Added Image generator

--- src/Service/Weather/ImageGenerator.php
+++ src/Service/Weather/ImageGenerator.php
@@ -18,14 +18,43 @@ abstract class ImageGenerator
         $im2 = imagecreatefrompng($img);
 
         imagecopy($this->image, $im2, $x, $y, 0, 0, imagesx($im2), imagesy($im2));
-        imagettftext($this->image, 12, 0, $x + imagesx($im2), $y + (imagesy($im2) / 2), 0, $fontFile, $name);
+        imagettftext($this->image, 12, 0, $x + imagesx($im2), $y + (imagesy($im2) / 2),
+            ImageColorAllocate($this->image, 186, 0, 0), $fontFile, $name);
         return $this;
     }
 
     protected function generateImage($date)
     {
-        $file = __DIR__ . '/../../../public/images/weather/' . $date . '.png';
+        $dir = date('Y-m-d_H');
+        $d = __DIR__ . '/../../../public/images/weather/' . $dir;
+        if (!is_dir($d)) {
+            mkdir($d);
+        }
+        $file = $d . '/' . $date . '.png';
         imagepng($this->image, $file);
-        return 'images/weather/' . $date . '.png';
+    }
+
+    protected function read()
+    {
+        $dir = __DIR__ . '/../../../public/images/weather/';
+        $dirs = $this->scandir($dir);
+        rsort($dirs);
+        $images = array_diff(scandir($dir . '/' . $dirs[0]), ['.', '..']);
+        foreach ($images as $key => $image) {
+            $images[$key] = 'images/weather/' . $dirs[0] . '/' . $image;
+        }
+        return $images;
+    }
+
+    protected function scandir($dir)
+    {
+        $dirs = array_diff(scandir($dir), ['.', '..']);;
+        $return = [];
+        foreach ($dirs as $d) {
+            if (is_dir($dir . $d)) {
+                $return[] = $d;
+            }
+        }
+        return $return;
     }
 }
--- src/Service/Weather/WeatherService.php
+++ src/Service/Weather/WeatherService.php
@@ -20,18 +20,27 @@ class WeatherService extends ImageGenerator
 
     protected function generate()
     {
-        for ($i = 0; $i < 1; $i++) {
-            $date = $i;
-            $this->image = imagecreatefrompng(__DIR__ . '/../../../files/weather/base1.png');
-            imagesavealpha($this->image, true);
-            foreach ($this->data as $record) {
-                $date = $record['list'][$i]['dt'];
-                $city = $record['city']['name'];
-                $icon = __DIR__ . '/../../../files/weather/icons/' . $record['list'][$i]['weather'][0]['icon'] . '.png';
-                $this->setCity($this->cities[$city]['x'], $this->cities[$city]['y'], $this->cities[$city]['name'], $icon);
+        $dirs = $this->scandir(__DIR__ . '/../../../public/images/weather/');
+        rsort($dirs);
+        $date = date('Y-m-d_H');
+        if (in_array($date, $dirs) && strtotime($date) > strtotime($dirs[0])){
+            for ($i = 0; $i < 7; $i++) {
+                $date = $i;
+                $this->image = imagecreatefrompng(__DIR__ . '/../../../files/weather/base.png');
+                imagesavealpha($this->image, true);
+                foreach ($this->data as $key => $record) {
+                    $date = $record['list'][$i]['dt'];
+                    $city = $record['city']['name'];
+                    $icon = __DIR__ . '/../../../files/weather/icons/' . $record['list'][$i]['weather'][0]['icon'] . '.png';
+                    $this->setCity($this->cities[$city]['x'], $this->cities[$city]['y'], $this->cities[$city]['name'],
+                        $icon);
+                }
+                imagettftext($this->image, 25, 0, 50, 50, ImageColorAllocate($this->image, 186, 0, 0),
+                    __DIR__ . '/../../../resources/fonts/roboto.ttf', date('d-m-Y H:i', $date));
+                $this->generateImage($date);
             }
-            $this->images[$date] = $this->generateImage($date);
         }
+        $this->images = $this->read();
     }
 
     private function loadData()
@@ -45,7 +54,7 @@ class WeatherService extends ImageGenerator
     private function getData($location)
     {
         $config = config();
-        $url = $config->get('weather.url.base') . 'forecast?q=' . $location . '&cnt=8&appid=' . $config->get('weather.key');
+        $url = $config->get('weather.url.base') . 'forecast/daily?q=' . $location . '&appid=' . $config->get('weather.key');
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
